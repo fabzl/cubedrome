9a2e4219e34631da06c9d598d3da189654e3a8a7
Fabian <fabian@fabz.tv>
2014-06-13 17:04:36 +0100

    lol -chat v000

--- index.js
+++ index.js
@@ -13,16 +13,19 @@ var ServerEvent =
 // Events we will listen for from socket.io.
 var SocketEvent =
 {
-	IO_CONNECT			:	"connection",
-	IO_DISCONNECT		:	"disconnect",
-	REQUEST_FORM_DATA	:	"requestFormData",
-	SAVE_FORM_DATA		:	"saveFormData",
-	REGISTER_USER_DATA	:	"registerUserData"
+	IO_CONNECT				:	"connection",
+	IO_DISCONNECT			:	"disconnect",
+	REGISTER_USER			:	"registerUser",
+	REGISTER_USER_ID		:	"registerUserID",
+	POPULATE_LIST_HEADER	: 	"populateListHeader",
+	USER_JOINED           :   "userJoined",
+    USER_LEFT             :   "userLeft",
+    USER_TYPING           :   "typing",
+    USER_STOP_TYPING      :   "stopTyping"
 };
 
-
 //-----------------------------------------------------------------------------//
-// required modules & important stuff 
+// required modules & important stuff
 //-----------------------------------------------------------------------------//
 
 var _mongodb							=	require ("mongodb"),
@@ -30,10 +33,7 @@ var _mongodb							=	require ("mongodb"),
 	app									=	express(),
 	port								=	3700,
 	_path								=	require ("path"),
-	_io									=	require('socket.io').listen(app.listen(port)),
-	_cirrusKey							=	"2bed2b368a970a3c0bdd7116-5d3d9fb4c37f",
-	_mongoServer						=	null,
-	_mongoClient						=	null;
+	_io									=	require('socket.io').listen(app.listen(port))
 
 
 //-----------------------------------------------------------------------------//
@@ -42,31 +42,16 @@ var _mongodb							=	require ("mongodb"),
 
 var	UserSchema								=	require("./schema"),
 	db										=	require('./db');
-	
+
 
 
 //-----------------------------------------------------------------------------//
 // logic private vars
 //-----------------------------------------------------------------------------//
-var _userIDArray						=	[],
-	_nickNameArray						=	[],
-	_firstNameArray						=	[],
-	_lastNameArray						=	[],
-	countryArray						=	[],
-	emailArray							=	[],
-	cirruIDArray						=	[],
-	sexArray							=	[],
-	dateOfBirthArray					=	[],
-	blackOrWhiteArray					=	[],
-	confirmedUserArray					=	[],
-	goldCubesArray						=	[],
-	colorModeArray						=	[],
-	scoreArray							=	[],
-	countryFilterArray					=	[],
-	itemsArray							=	[],
-	reportedArray						=	[],
-	penaltyOn							=	[],
+var _userName							=	[],
+	_mainUserDictionary					=	{},
 	usersConnected						=	0;
+
 
 
 
@@ -76,12 +61,12 @@ function init()
 	addSocketListeners();
 
 	//db stuff
-	db.connect();
-	exports = module.exports;
+	//db.connect();
+	//exports = module.exports;
 
 	// sending the index.html
 	console.log("Listening on port " + port);
-	app.configure (expressConfiguration);
+	app.configure(expressConfiguration);
 	app.use(express.static(__dirname + '/public'));
 
 }
@@ -89,9 +74,9 @@ function init()
 
 function addSocketListeners()
 {
-	// socket connection listeners 
+	// socket connection listeners
 	_io.sockets.on (SocketEvent.IO_CONNECT, socketIOConnectionHandler);
-	_io.sockets.on (SocketEvent.IO_DISCONNECT, socketIODisonnectHandler);
+	_io.sockets.on (SocketEvent.IO_DISCONNECT, socketIODisconnectHandler);
 
 }
 
@@ -100,10 +85,9 @@ function expressConfiguration ()
 {
 
 	app.use(express.static (_path.join (__dirname, "public")));
-	
+
 	// respond to web GET requests with the index.html page:
 	app.get ("/", routeRequestToindex);
-					 
 
 }
 
@@ -112,7 +96,6 @@ function routeRequestToindex (req, res)
 		res.sendfile (__dirname + "/index.html");
 		console.log("IM RUNNNING");
 }
-
 
 _io.sockets.on('connection', function (socket) {
 	socket.emit('message', { message: 'welcome to the chat' });
@@ -123,116 +106,181 @@ _io.sockets.on('connection', function (socket) {
 });
 
 
+
+  // Socket events
+
+  // Whenever the server emits 'login', log the login message
+  _io.sockets.on('login', function (data) {
+    connected = true;
+    // Display the welcome message
+    var message = "Welcome to Socket.IO Chat &mdash; ";
+    log(message, {
+      prepend: true
+    });
+    addParticipantsMessage(data);
+  });
+
+
+
 function socketIOConnectionHandler (socket)
 {
 	console.log ("Server:: [socketIOConnectionHandler]");
-	
+
 	usersConnected ++;
 
-	// socket.emit (SocketEvent.IO_CONNECT);
-	socket.on (SocketEvent.REGISTER_USER_DATA, saveFormDataHandler);
-	//socket.on (SocketEvent.REQUEST_FORM_DATA, requestFormDataHandler);
-	//socket.on (SocketEvent.SAVE_FORM_DATA, saveFormDataHandler);
+	socket.emit (SocketEvent.IO_CONNECT);
+	socket.on (SocketEvent.REGISTER_USER, storeUser);
+	socket.on (SocketEvent.IO_DISCONNECT, socketIODisconnectHandler);
+	//socket.on (SocketEvent.USER_JOINED, userJoined);
+	//socket.on (SocketEvent.USER_LEFT, userLeft);
+	//socket.on (SocketEvent.USER_TYPING, userTyping);
+	//socket.on (SocketEvent.USER_STOP_TYPING, userStopTyping);
+
+	var user = {
+
+		username 	: "",
+		userID 		: socket.id,
+		socket 		: socket,
+		lat 		: undefined,
+		lon 		: undefined,
+		nearbyUsers : [],
+		bannedUsers : [],
+
+	}
+	_mainUserDictionary[socket.id] = user;
+    socket.emit(SocketEvent.REGISTER_USER_ID, { userID : socket.id });
+
 }
 
-function socketIODisonnectHandler (socket)
+
+function socketIODisconnectHandler (socket)
 {
 	console.log ("Server:: [socketIODisonnectHandler]");
 
-
 	usersConnected --;
-	
+	if(_mainUserDictionary[socket.id]) {
+		delete _mainUserDictionary[socket.id];
+	//	updateUserInfo(_mainUserDictionarysocket.id);
+		socket.broadcast.emit(SocketEvent.USER_LEFT);
+	}
+
 	// socket.emit (SocketEvent.IO_DISCONNECT);
 }
 
-function requestFormDataHandler (formID, callback)
-{
-	console.log ("Server:: [requestFormDataHandler]");
 
-	_formsCollection.find ({formID:id}).toArray (toArrayHandler);
-
-	function toArrayHandler (err, arr)
-	{
-		callback (arr);
-	}
-}
+function createUsersList(activeUser) {
 
 
+	//console.log("createUsersList")
+	var distanceArray = [];
 
-function saveFormDataHandler (data)
-{
-	console.log ("Server:: [saveFormDataHandler]");
-	
-	//console.log(data);
-	var record = new UserSchema(data);
+	for (user in _mainUserDictionary) {
 
-	record.save(function(err) {
-		if(err){ 
-			console.log(err);
-		//	res.status(500).json({status: 'failure'})
-		}else{
-		//	res.json({status: 'success'});
+		//console.log(user);
+		var distance = getDistanceFromLatLonInKm(	activeUser.lat,
+													activeUser.lon,
+													_mainUserDictionary[user].lat*Math.random(),
+													_mainUserDictionary[user].lon*Math.random());
+
+		//console.log("user: "+_mainUserDictionary[user].username);
+		//	console.log("posi" ,activeUser.lat,activeUser.lon,_mainUserDictionary[user].lat,	_mainUserDictionary[user].lon);
+
+		var userDetails = {
+
+			userID 			: _mainUserDictionary[user].userID,
+			username		: _mainUserDictionary[user].username,
+			distanceArray	: distance
 		}
+		if ( activeUser.userID != userDetails.userID ) {
+			distanceArray.push(userDetails);
+		}
+	}
 
+	activeUser.nearbyUsers = distanceArray.sort(function(b, a) {
+
+		return a.distance - b.distance;
 	});
 
-	readFromDB();
+}
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+
+
+function storeUser(data) {
+
+	var user = 	_mainUserDictionary[data.userID];
+	user.username = data.username;
+	user.lat = data.userlatitude;
+	user.lon = data.userlongitude;
+
+
+
+	updateUserInfo(user);
+
 
 }
 
-function readFromDB() {
 
-	console.log("reading from DB");
-	console.log(UserSchema.find());
+function updateUserInfo(user) {
+
+	createUsersList(user);
+	sendUserData(user);
 
 }
 
 
 
+function sendUserData (user) {
 
-function checkIfFormDataExists (formID)
-{
-	console.log ("[checkIfFormDataExists]");
-				 
-	_formsCollection.find ({formID:formID}).toArray (formDataCollectionHandler);
-	
-	function formDataCollectionHandler (err, arr)
-	{
-		if (err)
-		{
-			// TODO : notify user and return;
-			console.log ("[formDataCollectionHandler] error: " + err);
-		}
-		
-		if (arr[0] === undefined)
-		{
-			console.log ("[formDataCollectionHandler] arr[0] is undefined, adding data to db...");
-			addFormDataToDB (data);
-		}
-		else
-		{
-			// TODO: create the updateFormData method.
-			// updateFormData (formID)
-		}
-	}
+	//console.log("sendUserData");
+	//console.log(user.socket);
+	//console.log("______");
+
+
+	//user.socket.broadcast.emit(SocketEvent.USER_JOINED, {
+
+
+
+	//});
+
+	// for(var i ; i < user.nearByUsers.length ; i ++  ) {
+
+	// 	_mainUserDictionary(user.nearbyUsers[i].userID).socket.emit(SocketEvent.USER_JOINED,{
+
+	// 		userID 		: user.userID,
+	// 		username 	: user.username,
+	// 		distance
+	// 	})
+
+	// }
+
+
+	user.socket.emit(SocketEvent.POPULATE_LIST_HEADER, {
+
+
+		username 		: user.username,
+		userID 			: user.userID,
+		usersConnected	: usersConnected,
+		nearbyUsers 	: user.nearbyUsers,
+	});
 }
 
-function addFormDataToDB (data)
-{
-	console.log ("[addPlayerToDB] writing name " + playerName + " to db...");
-	
-	var formDataStructureVO = _jsonStructureData.createFormDataStructureVO (data);
-	_formsCollection.insert (formDataStructureVO, formDataInsertHandler);
-}
-
-function formDataInsertHandler (err, doc)
-{
-	if (!err)
-	{
-		console.log ("[formDataInsertHandler] document added to db:");
-		console.dir (doc);
-	}
-}
 
 
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,47 +1,48 @@
 window.onload = function() {
- 
+
+
+    var SocketEvent =
+    {
+        REGISTER_USER_ID      :   "registerUserID",
+        POPULATE_LIST_HEADER  :   "populateListHeader",
+        USER_JOINED           :   "userJoined",
+        USER_LEFT             :   "userLeft",
+        USER_TYPING           :   "typing",
+        USER_STOP_TYPING      :   "stopTyping"
+
+    };
 
     var messages = [],
     socket = io.connect('http://localhost:3700'),
     field = document.getElementById("field"),
     sendButton = document.getElementById("sendButton"),
+    talkButton = document.getElementById("talkButton"),
     content = document.getElementById("content"),
     name = document.getElementById("name"),
-    sendRegister = document.getElementById("sendRegister"),
+    usernameDisplay= document.querySelector(".usernameDisplay"),
+    usersConnectedDisplay = document.querySelector(".usersConnectedDisplay"),
+    usersList = document.querySelector(".users-list"),
+    userlat,
+    userlong,
+    userName,
+    ID;
 
-    _userIDField                        =   document.getElementById("_userIDField"),
-    _nickNameField                       =   document.getElementById("_nickNameField"),
-    _firstNameField                      =   document.getElementById("_firstNameField"),
-    _lastNameField                       =   document.getElementById("_lastNameField"),
-    countryField                         =   document.getElementById("countryField"),
-    emailField                           =   document.getElementById("emailField"),
-    cirruIDField                         =   document.getElementById("cirruIDField "),
-    sexField                             =   document.getElementById("sexField"),
-    dateOfBirthField                     =   document.getElementById("dateOfBirthField"),
-    blackOrWhiteField                    =   document.getElementById("blackOrWhiteField"),
-    confirmedUserField                   =   document.getElementById("confirmedUserField"),
-    goldCubesField                       =   document.getElementById("goldCubesField"),
-    colorModeField                       =   document.getElementById("colorModeField"),
-    scoreField                           =   document.getElementById("scoreField"),
-    countryFilterField                   =   document.getElementById("countryFilterField"),
-    itemsField                           =   document.getElementById("itemsField"),
-    reportedField                        =   document.getElementById("reportedField"),
-    penaltyOnField                       =   document.getElementById("penaltyOnField"),
-    registerUserData                    =   "registerUserData";
-
- 
+    //listeners
+    socket.on(SocketEvent.REGISTER_USER_ID, getUserID);
+    socket.on(SocketEvent.POPULATE_LIST_HEADER, getUserData);
     socket.on('message', function (data) {
 
+    // to send message
 
-    //    console.log(data.message);    
-        
+    //    console.log(data.message);
+
         if(data.message) {
             messages.push(data);
             var html = '';
 
             for(var i=0; i<messages.length; i++) {
 
-            //    console.log(messages[i].message);  
+            //    console.log(messages[i].message);
             //    console.log(messages[i].username);
 
                 html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
@@ -52,74 +53,167 @@ window.onload = function() {
             console.log("There is a problem:", data);
         }
     });
- 
+
+    // Whenever the server emits 'user joined', log it in the chat body
+      socket.on('userJoined', function (data) {
+          console.log("user joined : "+data);
+            //    addParticipantsMessage(data);
+      });
+
+      // Whenever the server emits 'user left', log it in the chat body
+      socket.on('userLeft', function (data) {
+        console.log("stop typing : "+data);
+        //  addParticipantsMessage(data);
+        //removeChatTyping(data);
+      });
+
+      // Whenever the server emits 'typing', show the typing message
+    //  socket.on('typing', function (data) {
+       // addChatTyping(data);
+   //      console.log("typing : "+data);
+
+ //    });
+
+      // Whenever the server emits 'stop typing', kill the typing message
+ //     socket.on('stopTyping', function (data) {
+        //removeChatTyping(data);
+   //     console.log("stop typing : "+data);
+  //    });
+
+
 
     sendButton.onclick = function() {
 
-     //   console.log("namevalue");
-    //    console.log(name.value);
+
+       console.log("1click");
 
 
         if(name.value  === "")
         {
-            alert("Please type your name!");
+            alert("Please insert your a nickname!");
         } else {
-            var text = field.value;
-            socket.emit('send', { message: text, username: name.value });
+
+            userName = name.value;
+            getLocation();
 
         }
     };
 
-    sendRegister.onclick  = function() {
-    //    console.log("register");
-
-        var userData = {
-
-
-            _userID            :   _userIDField,             
-            _nickName          :   _nickNameField,        
-            _firstName         :  _firstNameField,               
-            _lastName          :  _lastNameField,                 
-            country            :  countryField,            
-            email             :   emailField,                   
-            cirruID           :   cirruIDField, 
-
-
-          //  sex               :                      
-            dateOfBirth       :   dateOfBirthField,             
-            blackOrWhite      :   blackOrWhiteField,            
-            confirmedUser     :   confirmedUserField,            
-            goldCubes         :   goldCubesField,           
-            colorMode         :   colorModeField,          
-            score             :   scoreField,      
-            countryFilter     :   countryFilterField,              
-            items             :   itemsField,             
-            reported          :   reportedField,           
-            penaltyOn         :   penaltyOnField          
-
-
-        } 
-        socket.emit(registerUserData,userData);
-
-        return false; 
-          }
-
-//console.log("sendRegister");
-//console.log(sendRegister);
-
-
     function dataExists (field) {
 
-
-     if(field.value == "") {
+        if(field.value == "") {
             field.value = "please fill"
             return false
         } else {
-           return field.value;
+            return field.value;
 
         }
     }
 
+    function getLocation() {
+           if (navigator.geolocation) {
+               navigator.geolocation.getCurrentPosition(showPosition);
+           } else {
+               console.log( "Geolocation is not supported by this browser.");
+               userlat =  undefined;
+               userlong = undefined;
+           }
+    }
+
+    function showPosition(position) {
+
+            userlat =  position.coords.latitude;
+            userlong = position.coords.longitude;
+           // console.log("Geolocation tracked, lat : "+userlat +"lon : "+userlong);
+            sendUserDetails();
+        }
+
+
+    function sendUserDetails() {
+
+
+         socket.emit('registerUser', {  userID: ID,
+                                        username: userName,
+                                        userlatitude: userlat,
+                                        userlongitude: userlong });
+
+
+    }
+
+
+    function getUserID (id) {
+
+    //   console.log("ID : "+id.userID);
+       ID = id.userID;
+
+    }
+
+
+    function getUserData(userData) {
+
+        console.dir(userData.nearbyUsers);
+        usernameDisplay.innerHTML = userData.username;
+        usersConnectedDisplay.innerHTML = "users: "+userData.usersConnected;
+
+       if(userData.nearbyUsers.length != 0 )  {
+
+            for(var i=0 ; i < userData.nearbyUsers.length ; i ++ ) {
+
+                var nearbyUser= document.createElement('li');
+                var nearbyUserText = userData.nearbyUsers[i].username;
+                console.log("NEARBYYYYYY USERNAME "+nearbyUserText);
+                console.log(nearbyUser);
+                nearbyUser.appendChild(nearbyUserText);
+
+               //     usersList.appendChild(nearbyUser);
+               // usersList.innerHTML=nearbyUser;
+            }
+        }else {
+           console.log("no nearby users");
+        }
+    }
+
+  // Whenever the server emits 'user joined', log it in the chat body
+  socket.on(SocketEvent.USER_JOINED, function (data) {
+
+    console.log(data + ' joined');
+  //  userJoined(data);
+
+  });
+
+
+  // Whenever the server emits 'user left', log it in the chat body
+  socket.on(SocketEvent.USER_LEFT, function (data) {
+    console.log(data.userID + ' left');
+  //  userLeft(data);
+   // removeChatTyping(data);
+  });
+
+  // Whenever the server emits 'typing', show the typing message
+  //socket.on(SocketEvent.USER_TYPING  , function (data) {
+    //addChatTyping(data);
+ //  console.log();
+//  });
+
+  // Whenever the server emits 'stop typing', kill the typing message
+ // socket.on(SocketEvent.USER_STOP_TYPING, function (data) {
+  // removeChatTyping(data);
+  //});
+
+
+
+
+function userJoined (data) {
+
+        console.log("userJoined"+data);
+
+
+    }
+function userLeft(data) {
+
+        console.log("userLeft"+data);
+
+    }
 
 
 
